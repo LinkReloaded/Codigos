d6d793f3211b6ffa3a7adfa9b7d4dee02f9613bf
LinkReloaded <linkreloaded@gmail.com>
2020-12-16 13:04:07 -0300

    Update imagen-destacada.php

--- wordpress/imagen-destacada.php
+++ wordpress/imagen-destacada.php
@@ -5,3 +5,12 @@
 
 <!-- busca si en el panel de control esta la imagen predeterminada, si no esta pone la que se inserta dentro del loop (USAR DENTRO DEL LOOP) -->
 <? if ( has_post_thumbnail() ) {?><a href="<? the_permalink(); ?>"><? the_post_thumbnail(); ?></a><? } else {cim_the_thumb('medium');} ?>
+
+<!-- imagen destacada URL -->
+<?php 
+  if(have_posts()) : while(have_posts()) : the_post(); 
+	  $thumb_id = get_post_thumbnail_id();
+		$thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true);
+		echo $thumb_url[0]; 
+  endwhile; endif; 
+?>
