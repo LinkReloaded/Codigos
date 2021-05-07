a5f097c77573801854abbfb5da47191c22451be9
LinkReloaded <linkreloaded@gmail.com>
2021-05-07 17:22:17 -0400

    Create fechas.php

---
+++ wordpress/fechas.php
@@ -0,0 +1,5 @@
+<!-- poner fechas en los post ej: MAYO, 05, 2021 -->
+<?php the_time('M'); ?>, <?php the_time('j'); ?>, <?php the_time('Y'); ?> 
+
+<!-- poner horas en los post ej: Publicado a las 19:15 -->
+Publicado a las <?php the_time('g:i'); ?>
